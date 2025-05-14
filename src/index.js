9837d7f2cda7ff18712332a3b253db167d62a67c
Harjyot Gulati <harjyotsinghgulati@gmail.com>
2025-05-14 23:50:15 +0530

    database connected

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("hello world.");
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+const port = process.env.PORT;
+app.listen(port,()=>{
+    console.log(`Applicatoin is running at PORT:${port}`);
+})
